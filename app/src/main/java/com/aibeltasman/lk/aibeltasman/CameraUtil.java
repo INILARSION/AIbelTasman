45bde7d378fc5104da34acf86f07a586e91206d4
LK <lars.karbach@web.de>
2018-10-10 16:08:47 +0200

    added classes to the CameraUtilIF, not implemented yet

--- app/src/main/java/com/aibeltasman/lk/aibeltasman/CameraUtil.java
+++ app/src/main/java/com/aibeltasman/lk/aibeltasman/CameraUtil.java
@@ -1,4 +1,112 @@
 package com.aibeltasman.lk.aibeltasman;
 
+import android.graphics.Bitmap;
+import android.hardware.Camera;
+
+import java.util.List;
+
 public class CameraUtil implements CameraUtilIF {
+
+
+    CameraUtil(){
+
+    }
+
+
+    @Override
+    public void closeCamera() {
+
+    }
+
+
+    @Override
+    public void startFlashlight() {
+
+    }
+
+
+    @Override
+    public void stopFlashlight() {
+
+    }
+
+
+    // retured FPS is FPS*1000 to fit in int
+    static List<int[]> getPreviewFpsRange(int cameraID){
+        return null;
+
+    }
+
+
+    // see here for formats: https://developer.android.com/reference/android/graphics/ImageFormat
+    static List<Integer> getSupportedPreviewFormats(int cameraID){
+        return null;
+    }
+
+
+    static List<Integer> getSupportedPreviewSizes(int cameraID) {
+        return null;
+    }
+
+
+    @Override
+    public Bitmap getPreviewBitmap() {
+        return null;
+    }
+
+
+    @Override
+    public int getBitmapWidth() {
+        return 0;
+    }
+
+
+    @Override
+    public int getBitmapHeight() {
+        return 0;
+    }
+
+
+    @Override
+    public int getPixel(int x, int y) {
+        return 0;
+    }
+
+
+    @Override
+    public int getRedPixel(int x, int y) {
+        return 0;
+    }
+
+
+    @Override
+    public int getGreenPixel(int x, int y) {
+        return 0;
+    }
+
+
+    @Override
+    public int getBluePixel(int x, int y) {
+        return 0;
+    }
+
+
+    // see here for formats: https://developer.android.com/reference/android/graphics/ImageFormat
+    @Override
+    public void setPreviewFormat(int format) {
+
+    }
+
+
+    // FPS parameters should be FPS*1000 to fit in int
+    @Override
+    public void setPreviewFpsRange(int min, int max) {
+
+    }
+
+
+    @Override
+    public void setPreviewSize(int width, int height) {
+
+    }
 }
--- app/src/main/java/com/aibeltasman/lk/aibeltasman/CameraUtilIF.java
+++ app/src/main/java/com/aibeltasman/lk/aibeltasman/CameraUtilIF.java
@@ -1,4 +1,34 @@
 package com.aibeltasman.lk.aibeltasman;
 
-public interface CameraUtilIF {
+import android.graphics.Bitmap;
+
+interface CameraUtilIF {
+
+    void closeCamera();
+
+    void startFlashlight();
+
+    void stopFlashlight();
+
+    Bitmap getPreviewBitmap();
+
+    int getBitmapWidth();
+
+    int getBitmapHeight();
+
+    int getPixel(int x, int y);
+
+    int getRedPixel(int x, int y);
+
+    int getGreenPixel(int x, int y);
+
+    int getBluePixel(int x, int y);
+
+    void setPreviewFormat(int format);
+
+    void setPreviewFpsRange(int min, int max);
+
+    void setPreviewSize(int width, int height);
+
+
 }
