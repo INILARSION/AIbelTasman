5228f0a7ca5fbcc1d6118257bde8c86a8a4fe54a
LK <lars.karbach@web.de>
2018-10-10 20:32:42 +0200

    added SoundUtilIF methods (not implemented) and sound files

--- app/src/main/java/com/aibeltasman/lk/aibeltasman/SoundUtilIF.java
+++ app/src/main/java/com/aibeltasman/lk/aibeltasman/SoundUtilIF.java
@@ -1,4 +1,14 @@
 package com.aibeltasman.lk.aibeltasman;
 
 public interface SoundUtilIF {
+
+    void playEngineSound();
+
+    void stopEngineSound();
+
+    void playBrakeSound();
+
+    void playCelebrateSound();
+
+    void playFailSound();
 }
