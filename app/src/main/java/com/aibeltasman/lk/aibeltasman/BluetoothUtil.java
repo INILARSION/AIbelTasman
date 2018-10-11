90b9332049c5b65d8c4456bd496183393b0e7131
LK <lars.karbach@web.de>
2018-10-11 18:54:52 +0200

    added classes to the BluetoothUtilIF, not implemented yet

--- app/src/main/java/com/aibeltasman/lk/aibeltasman/BluetoothUtil.java
+++ app/src/main/java/com/aibeltasman/lk/aibeltasman/BluetoothUtil.java
@@ -1,4 +1,48 @@
 package com.aibeltasman.lk.aibeltasman;
 
+import android.bluetooth.BluetoothDevice;
+
 public class BluetoothUtil implements BluetoothUtilIF {
+
+
+
+    public BluetoothUtil(){
+
+    }
+
+
+    @Override
+    public BluetoothDevice[] getPairedDevices() {
+        return new BluetoothDevice[0];
+    }
+
+    @Override
+    public String[] getPairedDevicesNames() {
+        return new String[0];
+    }
+
+
+    @Override
+    public void connectToDevice(BluetoothDevice device) {
+
+    }
+
+
+    @Override
+    public void sendData(byte[] buffer) {
+
+    }
+
+
+    @Override
+    public byte[] getData(int lenght) {
+        return new byte[0];
+    }
+
+
+    @Override
+    public void disconnect() {
+
+    }
+
 }
--- app/src/main/java/com/aibeltasman/lk/aibeltasman/BluetoothUtilIF.java
+++ app/src/main/java/com/aibeltasman/lk/aibeltasman/BluetoothUtilIF.java
@@ -1,4 +1,19 @@
 package com.aibeltasman.lk.aibeltasman;
 
+import android.bluetooth.BluetoothDevice;
+
 public interface BluetoothUtilIF {
+
+    BluetoothDevice[] getPairedDevices();
+
+    String[] getPairedDevicesNames();
+
+    void connectToDevice(BluetoothDevice device);
+
+    void sendData(byte[] buffer);
+
+    byte[] getData(int lenght);
+
+    void disconnect();
+
 }
