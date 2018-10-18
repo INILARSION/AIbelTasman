52009048d0eca7710fa2ec401244a5e8df3e7797
LK <lars.karbach@web.de>
2018-10-18 20:17:36 +0200

    Added MoveControl methods, not yet implemented because byte[] still unclear

--- app/src/main/java/com/aibeltasman/lk/aibeltasman/MoveControl.java
+++ app/src/main/java/com/aibeltasman/lk/aibeltasman/MoveControl.java
@@ -5,4 +5,42 @@ package com.aibeltasman.lk.aibeltasman;
  */
 
 public class MoveControl implements MoveControlIF {
+
+    private SendRcvIF sendRcv;
+
+    // Add static final byte[] here!
+
+    MoveControl(SendRcvIF sendRecv){
+        this.sendRcv = sendRecv;
+    }
+
+    @Override
+    public void driveForward() {
+
+    }
+
+    @Override
+    public void driveLeft() {
+
+    }
+
+    @Override
+    public void driveRight() {
+
+    }
+
+    @Override
+    public void turnLeft() {
+
+    }
+
+    @Override
+    public void turnRight() {
+
+    }
+
+    @Override
+    public void stop() {
+
+    }
 }
--- app/src/main/java/com/aibeltasman/lk/aibeltasman/MoveControlIF.java
+++ app/src/main/java/com/aibeltasman/lk/aibeltasman/MoveControlIF.java
@@ -1,4 +1,16 @@
 package com.aibeltasman.lk.aibeltasman;
 
 public interface MoveControlIF {
+
+    void driveForward();
+
+    void driveLeft();
+
+    void driveRight();
+
+    void turnLeft();
+
+    void turnRight();
+
+    void stop();
 }
