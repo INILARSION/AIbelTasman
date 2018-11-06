c588b0d9f965f1dc92ce3143356cfb5588cacc3f
LK <lars.karbach@web.de>
2018-11-06 15:22:48 +0100

    added color values from red tape/floor/table

--- app/src/main/java/com/aibeltasman/lk/aibeltasman/ImageRecognition.java
+++ app/src/main/java/com/aibeltasman/lk/aibeltasman/ImageRecognition.java
@@ -2,6 +2,31 @@ package com.aibeltasman.lk.aibeltasman;
 
 public class ImageRecognition implements ImageRecognitionIF {
 
+    /* --- COLOR VALUES ---
+    first two from LG G5
+    last two from Samsung S9
+
+    Red Tape:   R: 145 G: 36  B: 42
+                R: 129 G: 40  B: 46
+                R: 136  G: 13  B: 27
+                R: 135 G: 21  B: 35
+
+    Green Spot: R:  G:  B:
+                R:  G:  B:
+                R:  G:  B:
+                R:  G:  B:
+
+    Table:      R: 157 G: 154  B: 146
+                R: 160 G: 157  B: 149
+                R: 215 G: 215  B: 213
+                R: 210 G: 210  B: 208
+
+    Floor:      R: 170  G: 174 B: 175
+                R: 183  G: 191 B: 190
+                R: 129  G: 143 B: 133
+                R: 140  G: 145 B: 142
+     */
+
     private CameraUtil camera;
 
     public ImageRecognition(CameraUtil camera){
